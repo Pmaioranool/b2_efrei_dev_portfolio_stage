d53806272830a6643f02ae76ddad62bb717e89f4
Pmaioranool <lucasmaiorano77@gmail.com>
2025-01-13 17:44:26 +0100

    [Add] update header with title and CV download link; add CSS styles and home template

--- Templates/Partials/header.php
+++ Templates/Partials/header.php
@@ -4,13 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/Assets/CSS/style.css">
+    <title>Portfolio Lucas MAIORANO</title>
 </head>
 
 <body>
     <header>
         <nav>
             <a href="/">Accueil</a>
-            <a href="/a">Accueil</a>
+            <a href="\Assets\PDF\Lucas_MAIORANO_CV.pdf.pdf" download>télécharger mon CV</a>
         </nav>
     </header>
---
+++ Templates/home.php
@@ -0,0 +1,38 @@
+<main>
+    <article>
+        <section class="image-container">
+            <img src="/Assets/Images/profile/profileLoin.jpg" alt="photo de profile" class="image-profile">
+        </section>
+        <section>
+            <h2>Compétences</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>PHP</li>
+                <li>Symfony</li>
+                <li>Node.js</li>
+                <li>MySQL</li>
+                <li>PostgreSQL</li>
+                <li>Git</li>
+                <li>GitHub</li>
+            </ul>
+        </section>
+    </article>
+    <article class=".main-content">
+        <h1>Salut, moi c'est Lucas</h1>
+        <p>Je suis un développeur web junior, passionné par le développement web et mobile. J'ai commencé à apprendre le
+            développement web en école à EFREI en 2023. J'ai appris à développer des applications web et mobile en
+            utilisant des technologies modernes et des méthodes agiles. J'ai acquis des compétences en HTML, CSS,
+            JavaScript, PHP, Symfony, MySQL, PostgreSQL, Git, GitHub, et bien
+            d'autres.</p>
+        <p>Je suis actuellement à la recherche d'un stage et d'une alternance en tant que développeur web junior. Je
+            je suis ouvert à toutes les opportunités. Je suis prêt à relever de nouveaux défis et à apprendre de
+            nouvelles technologies. Je suis motivé, curieux, autonome, rigoureux, et j'aime travailler en équipe. Je
+            suis également passionné par la musique, les jeux vidéo, la lecture, les voyages et la cuisine.</p>
+        <p>Je suis disponible pour un entretien. N'hésitez pas à me contacter si vous avez des questions, des
+            suggestions, des opportunités.
+            si voici mon mail <a href="mailto:lucasmaiorano77@gmail.com">lucasmaiorano77@gmail.com</a> si possible
+            pouvez-vous mettre en objet : <strong>Proposition de stage ou d'alternance</strong></p>
+    </article>
+</main>
